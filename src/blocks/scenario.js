e5a1c3909e5d535794a6573a2f76b30c0fe3e536
Pierre Rolland <roll.pierre@gmail.com>
2017-05-04 22:53:15 +0200

    Edit scenario name

--- src/blocks/scenario.js
+++ src/blocks/scenario.js
@@ -7,16 +7,28 @@ export default class Scenario extends React.Component {
         super(props);
 
         this.state = {
-            scenario: this.props.scenario
+            scenario: this.props.scenario,
+            mode: 'read'
         };
 
+        this.switchToRead = this.switchToRead.bind(this);
+        this.switchToWrite = this.switchToWrite.bind(this);
         this.handleStepChange = this.handleStepChange.bind(this);
+        this.handleNameChange = this.handleNameChange.bind(this);
     }
 
     componentWillReceiveProps(nextProps) {
         if (JSON.stringify(nextProps.scenario) !== JSON.stringify(this.state.scenario)) {
             this.setState({ scenario: nextProps.scenario });
         }
+    }
+
+    switchToRead() {
+        this.setState({ mode: 'read' });
+    }
+
+    switchToWrite() {
+        this.setState({ mode: 'write' });
     }
 
     handleStepChange(e) {
@@ -29,10 +41,32 @@ export default class Scenario extends React.Component {
         });
     }
 
+    handleNameChange(e) {
+        let scenario = this.state.scenario;
+
+        scenario.name = e.target.value;
+        this.props.onChange({
+            scenario,
+            key: this.props.id
+        });
+    }
+
     render() {
         return (
             <div className={`scenario scenario--${this.state.scenario.type}`}>
-                <h2>{this.state.scenario.name}</h2>
+                <h2 className={`scenarioTitle scenarioTitle--${this.state.mode}Mode`} onClick={this.switchToWrite} onBlur={this.switchToRead}>
+                    {this.state.mode === 'read' ? (
+                        this.state.scenario.name
+                    ) : (
+                        <input
+                            type="text"
+                            className="scenario-titleInput"
+                            value={this.state.scenario.name}
+                            onChange={this.handleNameChange}
+                            autoFocus={true}
+                        />
+                    )}
+                </h2>
                 {this.state.scenario.steps.map((step, id) => (
                     <Step
                         step={step}
