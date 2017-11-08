d2f4ead53cba457a6f953a30c131e586f841f89c
Pierre Rolland <prolland@lafourchette.com>
2017-11-08 10:08:35 +0100

    Added branch handling

--- src/blocks/projectGitRepository.js
+++ src/blocks/projectGitRepository.js
@@ -8,41 +8,44 @@ export default class ProjectGitRepository extends React.Component {
 
         this.state = {
             displayed: false,
-            projectGitRepository: this.props.project.gitRepository
+            projectGitRepository: this.props.project.gitRepository,
+            projectGitBranch: this.props.project.gitBranch ? this.props.project.gitBranch : 'master'
         };
-
-        this.displayForm = this.displayForm.bind(this);
-        this.hideForm = this.hideForm.bind(this);
-        this.submitForm = this.submitForm.bind(this);
-        this.handleProjectGitRepositoryChange = this.handleProjectGitRepositoryChange.bind(this);
     }
 
     componentWillReceiveProps(nextProps) {
         if (JSON.stringify(nextProps.project) !== JSON.stringify(this.props.project)) {
-            this.setState({ projectGitRepository: nextProps.project.gitRepository });
+            this.setState({
+                projectGitRepository: nextProps.project.gitRepository,
+                projectGitBranch: nextProps.project.gitBranch ? nextProps.project.gitBranch : 'master'
+            });
         }
     }
 
-    displayForm() {
+    displayForm = () => {
         this.setState({ displayed: true });
-    }
+    };
 
-    hideForm() {
+    hideForm = () => {
         this.setState({ displayed: false });
-    }
+    };
 
-    submitForm() {
+    submitForm = () => {
         ProjectModel.edit({
             slug: this.props.project.slug,
             gitRepository: this.state.projectGitRepository
         }).then(() => {
             this.setState({ displayed: false });
         });
-    }
+    };
 
-    handleProjectGitRepositoryChange(e) {
+    handleProjectGitRepositoryChange = (e) => {
         this.setState({ projectGitRepository: e.target.value });
-    }
+    };
+
+    handleProjectGitBranchChange = (e) => {
+        this.setState({ projectGitBranch: e.target.value });
+    };
 
     render() {
         return (
@@ -54,10 +57,16 @@ export default class ProjectGitRepository extends React.Component {
                     <div className="projectGitRepository-form grid">
                         <input
                             type="text"
-                            placeholder="Project's git repository..."
+                            placeholder="Git repository..."
                             autoFocus={true}
                             onChange={this.handleProjectGitRepositoryChange}
                             value={this.state.projectGitRepository}
+                        />
+                        <input
+                            type="text"
+                            placeholder="Branch..."
+                            onChange={this.handleProjectGitBranchChange}
+                            value={this.state.projectGitBranch}
                         />
                         <div className="projectGitRepository-okButton" onClick={this.submitForm}>
                             <i className="fa fa-check" aria-hidden="true"> </i>
@@ -67,7 +76,9 @@ export default class ProjectGitRepository extends React.Component {
                         </div>
                     </div>
                 ) : (
-                    <div className="projectGitRepository-name">{this.state.projectGitRepository}</div>
+                    <div className="projectGitRepository-name">
+                        {this.state.projectGitRepository} ({this.state.projectGitBranch})
+                    </div>
                 )}
             </div>
         );
